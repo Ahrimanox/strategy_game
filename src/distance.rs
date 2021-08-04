0deca8491a36ac0350cc0040ed83fa7e93f3ee5d
Ahrimanox <clement.dubus@gmail.com>
2021-08-04 21:21:19 +0200

    Add NullDistance2D and EuclideanDistanceWHeight2D distances to use null heuristic and take height into account in shortest-path algorithm

--- src/distance.rs
+++ src/distance.rs
@@ -1,3 +1,5 @@
+use crate::map::Map;
+
 pub trait Distance2D {
     fn evaluate(&self, pos1: (f64, f64), pos2: (f64, f64)) -> f64;
 }
@@ -15,5 +17,30 @@ pub struct ManhattanDistance2D {}
 impl Distance2D for ManhattanDistance2D {
     fn evaluate(&self, pos1: (f64, f64), pos2: (f64, f64)) -> f64 {
         (pos2.0 - pos1.0).abs() + (pos2.1 - pos1.1).abs()
+    }
+}
+
+pub struct NullDistance2D {}
+
+impl Distance2D for NullDistance2D {
+    fn evaluate(&self, pos1: (f64, f64), pos2: (f64, f64)) -> f64 {
+        0.0
+    }
+}
+
+pub struct EuclideanDistanceWHeight2D<'d> {
+    pub height_map: &'d Map<f64>
+}
+
+impl<'d> Distance2D for EuclideanDistanceWHeight2D<'_> {
+    fn evaluate(&self, pos1: (f64, f64), pos2: (f64, f64)) -> f64 {
+        // Get associated height for the two position
+        let ipos1 = (pos1.0 as i32, pos1.1 as i32);
+        let ipos2 = (pos2.0 as i32, pos2.1 as i32);
+
+        let height1 = self.height_map[ipos1];
+        let height2 = self.height_map[ipos2];
+
+        ((pos2.0 - pos1.0).powi(2) + (pos2.1 - pos1.1).powi(2) + (height2 - height1).powi(2)).sqrt()
     }
 }
